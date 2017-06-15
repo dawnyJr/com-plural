3555ec768b74e8ccef7df7feb421579020843bb0
aelgdaouni <achraf.elgdaouni@gmail.com>
2017-06-15 02:04:52 +0200

    first commit

---
+++ src/main/java/com/plural/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.plural;
+
+public class HelloWorld {
+	
+public static void main(String[] args) {
+	System.out.println("Hello world :D");
+	System.out.println("dummy modification");
+}
+}
